913c8a9738c35540310b25715d1c9181fef87718
Kris Salerno <k.c.salerno@gmail.com>
2025-03-09 17:40:03 -0400

    DB Schema DDL create and delete
    Scripts for creating and deleting the retriever essentials DB. Next steps is mocked test data and a testing stored procedure.

---
+++ database/schemas-and-data/re_dropAll.sql
@@ -0,0 +1,16 @@
+-- This script will remove all traces of the 're_inventory' database and its data.
+USE re_inventory;
+
+-- Drop the Tables
+DROP TABLE IF EXISTS inventory_log;
+DROP TABLE IF EXISTS ordered_item;
+DROP TABLE IF EXISTS purchased_item;
+
+DROP TABLE IF EXISTS checkout_order;
+DROP TABLE IF EXISTS purchase_order;
+DROP TABLE IF EXISTS vendor;
+DROP TABLE IF EXISTS app_user;
+DROP TABLE IF EXISTS item;
+
+-- Drop the Database
+DROP DATABASE IF EXISTS re_inventory;
