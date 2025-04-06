6ffe7609d41b22348a7fdaa4fdeb60a314cd59c3
Kris Salerno <k.c.salerno@gmail.com>
2025-04-05 23:45:52 -0400

    Began Unit Testing Data Layer
    Added KnownGoodState class to be used for setting up testing prior to each unit test. Ran shouldFindAll to confirm connection and successful fetch, we are golden!

--- server/src/test/java/re/api/data/ItemJdbcTemplateRepositoryTest.java
+++ server/src/test/java/re/api/data/ItemJdbcTemplateRepositoryTest.java
@@ -1,13 +1,40 @@
 package re.api.data;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import re.api.models.Item;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ItemJdbcTemplateRepositoryTest {
+    private final int ITEM_COUNT = 37;
+
+    @Autowired
+    ItemJdbcTemplateRepository itemJdbcTemplateRepository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
+    }
 
     @Test
-    void findAll() {
+    void shouldFindAll() {
+        // Arrange
+        // Act
+         List<Item> items = itemJdbcTemplateRepository.findAll();
+        // Assert
+        assertNotNull(items);
+        assertFalse(items.isEmpty());
+        assertTrue(items.size() == ITEM_COUNT
+                || items.size() == ITEM_COUNT - 1); // Tests may run out of order; if delete is called, we may have one less item.
     }
 
     @Test
